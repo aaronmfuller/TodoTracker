da0546ee090e372a0b41b2c1b9e4d1f7487159ac
aaronmfuller <104102933+aaronmfuller@users.noreply.github.com>
2023-01-16 21:44:47 +0000

    fixed bug in delete button

--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,46 +1,43 @@
-import React, { useState } from "react";
-import TodoDate from "./TodoDate";
-import "./TodoItem.css";
+import React, { useState } from 'react';
+import TodoDate from './TodoDate';
+import './TodoItem.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
- const TodoItems=(props)=> {
-  
-const [todos, setTodos] = useState(props.todos);
+const TodoItems = (props) => {
+	const [todos, setTodos] = useState(props.todos);
 
-    
-  const handleDelete = (item) => {
-    console.log(item);
-    console.log("handle delete called...");
-    const todos = item.filter((t) => t.id !== item.id);
-    setTodos(todos);
-  };
- 
-    return (<table className="table table-striped">
-        <thead className="d-flex">
+	const handleDelete = (item) => {
+		// console.log(item);
+		// console.log('handle delete called...');
+    const newTodos = todos.filter((t) => t.id !== item.id);
+		setTodos(newTodos);
+	};
 
-        </thead>
-     <tbody>
-        {todos.map((item) => (
-          <tr key={item.id}><td>
-            <div className="todo-item__description">
-              <TodoDate date={item.date} />
+	return (
+		<table className='table table-striped'>
+			<thead className='d-flex'></thead>
+			  <tbody>
+				{todos.map((item) => (
+					<tr key={item.id}>
+						<td>
+							<div className='todo-item__description'>
+								<TodoDate date={item.date} />
 
-              <h2>{item.task}</h2>
-              <button
-                onClick={() => handleDelete(item)}
-                className="btn btn-danger btn-sm p-2 flex-shrink-1">
-                Delete
-              </button>
-            </div>
-            </td>
-
-          </tr>
-        ))}
-       
-       </tbody>
-      </table>
-    );
-  }
+								<h2>{item.task}</h2>
+								<button
+									onClick={() => handleDelete(item)}
+									className='btn btn-danger btn-sm p-2 flex-shrink-1'
+								>
+									Delete
+								</button>
+							</div>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
+};
 
 export default TodoItems;
